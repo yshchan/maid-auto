fabcfd2de45171edc9637f9a7366cd8173fd3bbf
Yashwant Chauhan <yashwantc90@gmail.com>
2011-11-19 22:25:33 +0100

    Updated Ruby Files

--- Ruby Files/maid-setup.rb
+++ Ruby Files/maid-setup.rb
@@ -2,6 +2,6 @@
 
 install = `sudo gem install maid`
 make = `maid sample`
-rename = File.rename( "~/.maid/rules.sample.rb", "~/.maid/rules.rb" )
+rename = `cd ~/.maid; mv rules.sample.rb rules.rb`
 
 puts install + make + rename
